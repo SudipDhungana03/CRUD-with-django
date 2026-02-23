cc74bec2fec0583536e1a936564f9dcd9f10cd60
SudipDhungana03 <sudipdhungana2063@gmail.com>
2026-02-23 20:32:25 +0545

    Update settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'grocery',
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
