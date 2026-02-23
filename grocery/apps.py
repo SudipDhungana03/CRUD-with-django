4df3a29b3c03e0cd073d0f1c601069a1270ccdf9
SudipDhungana03 <sudipdhungana2063@gmail.com>
2026-02-23 21:59:34 +0545

    Move Procfile to root and remove extra folder

--- grocery/apps.py
+++ grocery/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class GroceryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'grocery'
+    name = 'grocery' # <--- This name must match your folder name exactly
